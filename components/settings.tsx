bf46f9789cd46a2435c212abe8e15271a63f6e2d
AmberTJC <147658220+AmberTJC@users.noreply.github.com>
2025-03-20 14:20:44 -0600

    Added toggles and adjusted styling

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,32 +1,64 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function SettingsPage()
 {
+
+    const [stickyRepeat, setStickyRepeat] = useState(false);
+    const [gaplessPlayback, setGaplessPlayback] = useState(false);
+    const [displayMode, setDisplayMode] = useState(false);
+
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                     <Text style={styles.headerText}>PlayStream</Text>
                   </View>
             <View>
+                <View style = {styles.row}>
                 <Ionicons style ={styles.icons} name = "person-circle-outline" size={40} color="#0D9488"></Ionicons>
                 <Text style = {styles.sectionTitle}>Account Settings</Text>
+                </View>
                 <Text style = {styles.subText}>Manage Account</Text>
             </View>
             <View>
+                <View style = {styles.row}>
                 <Ionicons style ={styles.icons} name = "repeat-outline" size={40} color="#0D9488"></Ionicons>
                 <Text style = {styles.sectionTitle}>Sticky Repeat</Text>
+                <Switch
+                            value={stickyRepeat}
+                            onValueChange={setStickyRepeat}
+                            thumbColor={stickyRepeat ? "#0D9488" : "#888"}
+                            trackColor={{ false: "#333", true: "#0D9488" }}
+                        />
+                </View>
                 <Text style = {styles.subText}>Repeat persists between song selections</Text>
             </View>
             <View>
+                <View style = {styles.row}>
             <Ionicons style ={styles.icons} name = "sync-outline" size={40} color="#0D9488"></Ionicons>
                 <Text style = {styles.sectionTitle}>Gapless Playback</Text>
+                <Switch
+                            value={gaplessPlayback}
+                            onValueChange={setGaplessPlayback}
+                            thumbColor={gaplessPlayback ? "#0D9488" : "#888"}
+                            trackColor={{ false: "#333", true: "#0D9488" }}
+                        />
+                </View>
                 <Text style = {styles.subText}>Plays without pausing between songs</Text>
             </View>
             <View>
+                <View style = {styles.row}>
             <Ionicons style ={styles.icons} name = "toggle-outline" size={40} color="#0D9488"></Ionicons>
                 <Text style = {styles.sectionTitle}>Display</Text>
+                <Switch
+                            value={displayMode}
+                            onValueChange={setDisplayMode}
+                            thumbColor={displayMode ? "#0D9488" : "#888"}
+                            trackColor={{ false: "#333", true: "#0D9488" }}
+                        />
+                </View>
                 <Text style = {styles.subText}>Toggles between dark and light mode</Text>
             </View>
         </View>
@@ -58,15 +90,25 @@ const styles = StyleSheet.create({
         color: '#0D9488',
         fontSize: 18,
         fontWeight: '600',
-        marginBottom: 10,
+        marginLeft: 10,
+        marginTop: 15,
+        flex: 1,
+        flexShrink: 1,
       },
       subText: {
         color: '#0D9488',
         fontSize: 14,
         marginBottom: 40,
+        marginLeft: 50,
       },
       icons: {
         marginTop: 15,
-        
       },
+
+      row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+      }
 });
